477130f3185bc7216b6a83d7fc61eee89a327d30
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-03-17 15:58:26 +0900

    update coordinate

--- src/models/coordinate.js
+++ src/models/coordinate.js
@@ -1,8 +1,8 @@
 /**
- * @param {number} p1X - float position 1 x
- * @param {number} p1Y - float position 1 y
- * @param {number} p2X - float position 2 x
- * @param {number} p2Y - float position 2 y
+ * @param {number} p1X - position 1 x
+ * @param {number} p1Y - position 1 y
+ * @param {number} p2X - position 2 x
+ * @param {number} p2Y - position 2 y
  * @return {number}
  */
 export const coordinateLength = (p1X, p1Y, p2X, p2Y) => {
@@ -10,30 +10,30 @@ export const coordinateLength = (p1X, p1Y, p2X, p2Y) => {
 }
 
 /**
- * @param {number[]} cTL - float[0,1] color top left
- * @param {number[]} cTR - float[0,1] color top right
- * @param {number[]} cBL - float[0,1] color bottom left
- * @param {number[]} cBR - float[0,1] color bottom right
- * @param {number} pRX - float[-1,1] position ratio x
- * @param {number} pRY - float[-1,1] position ratio y
- * @return {number[]} - float[0,1] pickup color
+ * @param {number[]} cTL - [0,1] color top left
+ * @param {number[]} cTR - [0,1] color top right
+ * @param {number[]} cBL - [0,1] color bottom left
+ * @param {number[]} cBR - [0,1] color bottom right
+ * @param {number} pRX - [-1,1] position ratio x
+ * @param {number} pRY - [-1,1] position ratio y
+ * @return {number[]} - [0,1] pickup color
  */
 export const coordinatePickupColor = (cTL, cTR, cBL, cBR, pRX, pRY) => {
   // color top
-  const _cT /* :number[] float[0,1] */ = cTL.map((
-    c /* :number float[0,1] */,
+  const _cT /* :number[] [0,1] */ = cTL.map((
+    c /* :number [0,1] */,
     i /* :number int[0,inf) */
   ) => {
     return cTL[i] + (cTR[i] - cTL[i]) * pRX
   })
   // color bottom
-  const _cB /* :number[] float[0,1] */ = cBL.map((
-    c /* :number float[0,1] */,
+  const _cB /* :number[] [0,1] */ = cBL.map((
+    c /* :number [0,1] */,
     i /* :number int[0,inf) */
   ) => {
     return cBL[i] + (cBR[i] - cBL[i]) * pRX
   })
-  return _cT.map((c /* :number float[0,1] */, i /* :number int[0,inf) */) => {
+  return _cT.map((c /* :number [0,1] */, i /* :number int[0,inf) */) => {
     return _cT[i] + (_cB[i] - _cT[i]) * pRY
   })
 }
