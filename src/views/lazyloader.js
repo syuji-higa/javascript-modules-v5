58550e37599f9473569baa6a950526b3613af4c6
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-03-21 21:17:24 +0900

    update lazyloader

--- src/views/lazyloader.js
+++ src/views/lazyloader.js
@@ -44,7 +44,7 @@ class Lazyloader {
   create() {
     this._$$el = document.getElementsByClassName(this._selfClassName)
     this._observer = new IntersectionObserver(this._inviewport.bind(this), {
-      rootMargin: '50% 50% 50% 50%'
+      rootMargin: '-10% 0 -10% 0'
     })
     return this
   }
