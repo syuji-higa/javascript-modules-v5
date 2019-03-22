75d9d2619be1573f33837e88c4066f1011318eb6
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-03-22 17:22:35 +0900

    add ScrollObserver

---
+++ src/modules/scroll-observer.js
@@ -0,0 +1,52 @@
+/**
+ * depends on 'vanix' used in '../store'
+ */
+
+import { store } from '../store'
+import { eventer } from '../modules'
+import { throttle } from '../utils/throttle'
+
+class ScrollObserver {
+  _scrollThrottle /* :function */
+  _scrollEvent /* :Object */ = {}
+
+  constructor() {
+    this._scrollThrottle = throttle(200)
+    this._scrollEvent = eventer.create(
+      window,
+      'scroll',
+      this._onScroll.bind(this)
+    )
+  }
+
+  /**
+   * @return {Instance}
+   */
+  on() {
+    this._scrollEvent.add()
+    return this
+  }
+
+  /**
+   * @return {Instance}
+   */
+  off() {
+    this._scrollEvent.remove()
+    return this
+  }
+
+  /**
+   * @return {Instance}
+   */
+  update() {
+    store.commit('setWindowOffsetX', window.pageXOffset)
+    store.commit('setWindowOffsetY', window.pageYOffset)
+    return this
+  }
+
+  _onScroll() {
+    this._scrollThrottle(this.update.bind(this))
+  }
+}
+
+export { ScrollObserver as default }
