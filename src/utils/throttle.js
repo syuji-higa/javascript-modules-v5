8b16bc54f12bb5baedcc95e34dbaa492ef6687fb
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-03-22 17:21:59 +0900

    add throttle

---
+++ src/utils/throttle.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Object} [options]
+ * @param {number} [options.interval] // int[0,inf)
+ * @param {boolean} [options.isLastRun]
+ * @return {function(fn: function)}
+ */
+export const throttle = (options = {}) => {
+  const { interval, isLastRun } = Object.assign(
+    { interval: 100, isLastRun: true },
+    options
+  )
+
+  let _lastTime /* :number int[0,inf) */ = new Date().getTime() - interval
+  let _timer /* :number int[0,inf) */ = 0
+
+  return (fn) => {
+    if (_lastTime + interval <= new Date().getTime()) {
+      _lastTime = new Date().getTime()
+      fn()
+    }
+    if (isLastRun) {
+      clearTimeout(_timer)
+      _timer = setTimeout(() => {
+        fn()
+        _timer = 0
+      }, interval)
+    }
+  }
+}
