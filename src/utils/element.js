e5ec1f8d84954211415671e5e839af839b3ec8c5
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-03-23 12:53:00 +0900

    update element

---
+++ src/utils/element.js
@@ -0,0 +1,65 @@
+import { kebabToPascalCase } from '../models/string'
+
+/**
+ * @param {Element} $el
+ * @param {string} selector
+ * @param {boolean} [isSlefTarget]
+ * @return {?Element}
+ */
+export const includesElement = ($el, selector, isSlefTarget = true) => {
+  const _elms /* :Element[] */ = Array.from(document.querySelectorAll(selector))
+  const _$el /* :Element */ = isSlefTarget ? $el : $el.parentNode
+
+  const _getParent = ($el /* :Element */) /* :Element */ => {
+    if (!$el || !$el.tagName || $el.tagName === 'HTML') return null
+
+    if (_elms.includes($el)) {
+      return $el
+    }
+
+    return _getParent($el.parentNode)
+  }
+
+  return _getParent(_$el)
+}
+
+/**
+ * @param {Elemtnt} el
+ * @param {string} dataName
+ * @param {Object} [options]
+ * @param {boolean} [options.needsSingle]
+ * @param {?function} [options.filter]
+ * @return {Object}
+ */
+export const groupByDataElement = (el, dataName, options = {}) => {
+  const { needsSingle, filter } = Object.assign(
+    {
+      needsSingle: false,
+      filter: null
+    },
+    options
+  )
+
+  const _$$el /* :HTMLCollection|NodeList */ = el.querySelectorAll(
+    `[data-${dataName}]`
+  )
+  return Array.from(_$$el).reduce((memo /* :Object */, $el /* :Element */) => {
+    if (!filter || filter($el)) {
+      const _dataName /* :string */ =
+        'dataset' in $el
+          ? $el.dataset[kebabToPascalCase(dataName)]
+          : $el.getAttribute(`data-${dataName}`)
+      if (_dataName) {
+        if (needsSingle) {
+          memo[_dataName] = $el
+        } else {
+          if (!memo[_dataName]) {
+            memo[_dataName] = []
+          }
+          memo[_dataName].push($el)
+        }
+      }
+    }
+    return memo
+  }, {})
+}
