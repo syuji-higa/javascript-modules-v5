4717eb9a4b618fc29eb5a1f3bb09d7240d3a13a8
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-03-24 01:13:12 +0900

    add ControlledRequestAnimationFramer

---
+++ src/utils/controlled-request-animation-framer.js
@@ -0,0 +1,83 @@
+class ControlledRequestAnimationFramer {
+  _animes /* :Map */ = new Map()
+  _animateHandle /* :number int[0,inf) */ = 0
+
+  /**
+   * @param {Object} key
+   * @return {number} [0,inf)
+   */
+  getTime(key) {
+    return this._animes.get(key).time
+  }
+
+  /**
+   * @param {Object} key
+   * @param {function} func
+   * @param {number} fps - int[0,inf)
+   * @param {number} [interval] - int[0,inf)
+   * @return {Instance}
+   */
+  add(key, func, fps, interval = 0) {
+    this._animes.set(key, {
+      startTime /* :number [0,inf) */: new Date().getTime(),
+      time /* :number [0,inf) */: 0,
+      frameStartTime /* :number [0,inf) */: 0,
+      ms /* :number [0,inf) */: fps === 'auto' ? 0 : Math.floor(1000 / fps),
+      count /* :number int[0,inf) */: 0,
+      interval /* :number int[0,inf) */: interval + 1,
+      func /* :function */: func
+    })
+
+    if (!this._animateHandle) {
+      this._start()
+    }
+
+    return this
+  }
+
+  /**
+   * @param {Object} key
+   * @return {Instance}
+   */
+  remove(key) {
+    this._animes.delete(key)
+    if (!this._animes.size) {
+      this._stop()
+    }
+
+    return this
+  }
+
+  _start() {
+    if (this._animateHandle) {
+      return
+    }
+    this._animate()
+  }
+
+  _stop() {
+    cancelAnimationFrame(this._animateHandle)
+    this._animateHandle = 0
+  }
+
+  _animate() {
+    this._animateHandle = requestAnimationFrame(this._animate.bind(this))
+
+    const _time = new Date().getTime()
+
+    this._animes.forEach(
+      ({ frameStartTime, ms, count, interval, func }, key) => {
+        const _anime /* :Object */ = this._animes.get(key)
+        _anime.count++
+        _anime.time = _time - _anime.startTime
+        if (count % interval || ms > _time - frameStartTime) {
+          return
+        }
+        _anime.frameStartTime = _time
+        func()
+      }
+    )
+  }
+}
+
+export { ControlledRequestAnimationFramer as default }
