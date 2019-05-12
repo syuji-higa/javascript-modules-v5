ea1f2797ba7684b6657949480e5d01a4a702c0bb
比嘉 秀士 <higa_syuuji@liginc.co.jp>
2019-05-12 14:51:22 +0900

    update store

--- src/store.js
+++ src/store.js
@@ -11,40 +11,45 @@ const bowser /* :Object */ = Bowser.getParser(window.navigator.userAgent)
 const { browser, engin, os, platform } = bowser.parsedResult
 
 const state /* :Object */ = {
+  // assetsDir /* :String */: '/assets',
   browser /* :Object */,
   engin /* :Object */,
   os /* :Object */,
   platform /* :Object */,
   breakPoint /* :number[] - int[0,inf) */: [768, 1024],
-  windowWidth /* :number[] - int[0,inf) */: 0,
-  windowHeight /* :number[] - int[0,inf) */: 0,
-  windowWidthLastChangedHeight /* :number[] - int[0,inf) */: 0,
+  windowWidth /* :number - int[0,inf) */: 0,
+  windowHeight /* :number - int[0,inf) */: 0,
+  windowWidthLastChangedHeight /* :number - int[0,inf) */: 0,
   windowSizeType /* :string */: '',
-  windowOffsetX /* :number[] - int[0,inf) */: 0,
-  windowOffsetY /* :number[] - int[0,inf) */: 0
+  windowOffsetX /* :number - int[0,inf) */: 0,
+  windowOffsetY /* :number - int[0,inf) */: 0,
+  inviewports /* :string[] */: []
 }
 
 const mutations /* :Object */ = {
-  setWindowWidth(state /* :Object */, data /* :number[] - int[0,inf) */) {
+  setWindowWidth(state /* :Object */, data /* :number - int[0,inf) */) {
     state.windowWidth = data
   },
-  setWindowHeight(state /* :Object */, data /* :number[] - int[0,inf) */) {
+  setWindowHeight(state /* :Object */, data /* :number - int[0,inf) */) {
     state.windowHeight = data
   },
   setWindowWidthLastChangedHeight(
     state /* :Object */,
-    data /* :number[] - int[0,inf) */
+    data /* :number - int[0,inf) */
   ) {
     state.windowWidthLastChangedHeight = data
   },
-  setWindowSizeType(state /* :Object */, data /* :number[] - int[0,inf) */) {
+  setWindowSizeType(state /* :Object */, data /* :number - int[0,inf) */) {
     state.windowSizeType = data
   },
-  setWindowOffsetX(state /* :Object */, data /* :number[] - int[0,inf) */) {
+  setWindowOffsetX(state /* :Object */, data /* :number - int[0,inf) */) {
     state.windowOffsetX = data
   },
-  setWindowOffsetY(state /* :Object */, data /* :number[] - int[0,inf) */) {
+  setWindowOffsetY(state /* :Object */, data /* :number - int[0,inf) */) {
     state.windowOffsetY = data
+  },
+  setInviewports(state /* :Object */, data /* :string */) {
+    state.inviewports = data
   }
 }
 
