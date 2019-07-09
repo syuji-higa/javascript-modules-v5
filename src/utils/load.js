e534641fe4c5236c8074eaf4bb6a5bea929e02ef
Syuji Higa <higa@SyujinoMacBook-puro.local>
2019-07-09 12:13:20 +0900

    update load

--- src/utils/load.js
+++ src/utils/load.js
@@ -16,6 +16,43 @@ export const loadVideo = ($video) => {
 }
 
 /**
+ * @param {Element} $el
+ * @param {Object} [options]
+ * @param {funciton} [options.done]
+ * @param {funciton} [options.fail]
+ * @param {funciton} [options.always]
+ * @return {Promise}
+ */
+const load = ($el, options = {}) => {
+  const { done, fail, always } = options
+
+  return new Promise((resolve) => {
+    const _always /* :function */ = (
+      $el /* :Element */,
+      isSuccess /* :boolean */
+    ) => {
+      if (always) {
+        always($el)
+      }
+      resolve({ $el, isSuccess })
+    }
+
+    $el.onload = () => {
+      if (done) {
+        done($el)
+      }
+      _always($el, true)
+    }
+    $el.onerror = () => {
+      if (fail) {
+        fail($el)
+      }
+      _always($el, false)
+    }
+  })
+}
+
+/**
  * @param {string} src
  * @param {Object} [options]
  * @param {funciton} [options.done]
@@ -24,60 +61,35 @@ export const loadVideo = ($video) => {
  * @return {Promise}
  */
 export const loadImage = (src, options = {}) => {
-  const { done, fail, always } = options
+  const _img /* :Image */ = new Image()
 
   return new Promise((resolve) => {
-    const _img /* :Image */ = new Image()
-
-    const _always = (img /* :Image */, isSuccess /* :boolean */) => {
-      if (always) always(img)
-      resolve(img, isSuccess)
-    }
-
-    _img.onload = () => {
-      if (done) done(_img)
-      _always(_img, true)
-    }
-    _img.onerror = () => {
-      if (fail) fail(_img)
-      _always(_img, false)
-    }
+    ;(async () => {
+      const { $el, isSuccess } = load(_img, options)
+      resolve({ $el, isSuccess })
+    })()
 
     _img.src = src
   })
 }
 
 /**
- * @param {string} file
+ * @param {string} src
  * @param {Object} [options]
  * @param {funciton} [options.done]
  * @param {funciton} [options.fail]
  * @param {funciton} [options.always]
  * @return {Promise}
  */
-export const loadFile = (file, options = {}) => {
-  const { done, fail, always } = options
+export const loadFile = (src, options = {}) => {
+  const _reader /* :FileReader */ = new FileReader()
 
   return new Promise((resolve) => {
-    const _reader /* :FileRender */ = new FileReader()
+    ;(async () => {
+      const { $el, isSuccess } = load(_img, options)
+      resolve({ $el, isSuccess })
+    })()
 
-    const _always /* :function */ = (
-      file_ /* :FileRender */,
-      isSuccess /* :boolean */
-    ) => {
-      if (always) always(file_)
-      resolve(isSuccess)
-    }
-
-    _reader.onload = (file_) => {
-      if (done) done(file_)
-      _always(file_, true)
-    }
-    _reader.onerror = (file_) => {
-      if (fail) fail(file_)
-      _always(file_, false)
-    }
-
-    _reader.readAsDataURL(file)
+    _reader.readAsDataURL(src)
   })
 }
